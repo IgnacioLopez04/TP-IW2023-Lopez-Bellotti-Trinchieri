d5efaed7e6693dec58bafe6a7aeca465f7fe92cb
Ignacio <ignacioelopez04@gmail.com>
2023-08-26 17:50:44 -0300

    CSS y HTML de login y signup. Falta terminar detalles de esos, y tambien lo que tenga que ver con la creacion de viajes

--- PlanificadorViajes/registration/forms.py
+++ PlanificadorViajes/registration/forms.py
@@ -8,3 +8,4 @@ class UserRegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+        help_text = {k:'' for k in fields} 
