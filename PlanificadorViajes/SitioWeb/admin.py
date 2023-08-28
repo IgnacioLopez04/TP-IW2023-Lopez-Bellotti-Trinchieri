ac5a3970970787240bb13b5d5c79afa42ad1307a
MarcosBellotti <marcosbellotti5@gmail.com>
2023-08-28 15:23:07 -0300

    Borre persona de model y de admin de SitioWeb porque no sirve

--- PlanificadorViajes/SitioWeb/admin.py
+++ PlanificadorViajes/SitioWeb/admin.py
@@ -1,8 +1,3 @@
 from django.contrib import admin
-from SitioWeb.models import Persona
+
 # Register your models here.
-
-class AdminPersona(admin.ModelAdmin):
-    list_display = ('nombreUsuario', 'email',)
-
-admin.site.register(Persona, AdminPersona)
--- PlanificadorViajes/SitioWeb/models.py
+++ PlanificadorViajes/SitioWeb/models.py
@@ -1,8 +1,5 @@
 from django.db import models
 
 # Create your models here.
-class Persona(models.Model):
-    nombreUsuario = models.CharField(max_length=50)
-    email = models.CharField(max_length=200, default="ejemplomail@ejemplo.com")
-    password = models.CharField(max_length=200, default="1234")
+
     
