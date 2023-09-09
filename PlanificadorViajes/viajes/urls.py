b25edf3a530597c5c20b0c712fdcc39575fd7541
LucasTrinchieri <lucastrinchieri123@gmail.com>
2023-09-09 20:48:29 -0300

    Eliminacion de formset.py y dia_viaje.html. Se modifico viajes/urls eliminando la referencia a formset

--- PlanificadorViajes/viajes/urls.py
+++ PlanificadorViajes/viajes/urls.py
@@ -1,11 +1,7 @@
 from django.urls import path
 from . import views
-from .formsets import FormsetDiaViaje
 
 urlpatterns = [
     path('', views.cargarViaje, name='viajes-cargar-viaje'),
     path('detalle-viaje/<int:viaje_id>/', views.detalle_viaje, name='detalle-viaje'),
-
-    #FORMSET
-    path('cargar-dia/<int:viaje_id>/', FormsetDiaViaje.as_view(), name='viajes-cargar-dia-viaje'),
 ]
