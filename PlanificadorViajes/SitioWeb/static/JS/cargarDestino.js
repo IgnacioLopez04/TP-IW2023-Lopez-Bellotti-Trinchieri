f6bfc2e84298ed65e0c8fe46ca8c3da63d6a177e
Ignacio <ignacioelopez04@gmail.com>
2023-10-04 20:19:53 -0300

    Modifique los botones para elimianr los emial en viajes y cargar destinos

--- PlanificadorViajes/SitioWeb/static/JS/cargarDestino.js
+++ PlanificadorViajes/SitioWeb/static/JS/cargarDestino.js
@@ -100,19 +100,20 @@ document.addEventListener('DOMContentLoaded', function () {
         }
 
         // Crear un elemento de texto para mostrar el correo
+        var elementoDiv = document.createElement('div');
+        elementoDiv.className = 'div-correo'
         var elementoLocalidad = document.createElement("span");
         elementoLocalidad.textContent = localidad.value + ', ' + provincia.value;
         listaDestinos.push(elementoLocalidad.textContent);
         console.log(listaDestinos);
         // Crear un botón de eliminar
         var botonEliminar = document.createElement("button");
-        botonEliminar.textContent = "Eliminar";
+        botonEliminar.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M256 48a208 208 0 1 1 0 416 208 208 0 1 1 0-416zm0 464A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM175 175c-9.4 9.4-9.4 24.6 0 33.9l47 47-47 47c-9.4 9.4-9.4 24.6 0 33.9s24.6 9.4 33.9 0l47-47 47 47c9.4 9.4 24.6 9.4 33.9 0s9.4-24.6 0-33.9l-47-47 47-47c9.4-9.4 9.4-24.6 0-33.9s-24.6-9.4-33.9 0l-47 47-47-47c-9.4-9.4-24.6-9.4-33.9 0z"/></svg>';
         botonEliminar.className = "btn-eliminar";
 
         // Manejar el clic en el botón de eliminar
         botonEliminar.addEventListener("click", function () {
-            destinosAgregados.removeChild(elementoLocalidad);
-            destinosAgregados.removeChild(botonEliminar);
+            destinosAgregados.removeChild(elementoDiv);
             listaDestinos.removeChild(elementoLocalidad);
 
 
@@ -124,8 +125,9 @@ document.addEventListener('DOMContentLoaded', function () {
             }
         });
 
-        destinosAgregados.appendChild(elementoLocalidad);
-        destinosAgregados.appendChild(botonEliminar);
+        elementoDiv.appendChild(elementoLocalidad);
+        elementoDiv.appendChild(botonEliminar);
+        destinosAgregados.appendChild(elementoDiv);
 
         // Agregar una coma y un espacio para separar múltiples correos
         destinosAgregados.appendChild(document.createTextNode(", "));
