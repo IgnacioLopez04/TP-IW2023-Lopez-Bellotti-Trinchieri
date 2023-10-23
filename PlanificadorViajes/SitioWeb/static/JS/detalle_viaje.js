dc543c8c2a9f4f9bc32c27cf1c09a91189f8d9ea
Ignacio <ignacioelopez04@gmail.com>
2023-10-23 00:14:29 -0300

    *Agregado de una galeria de fotos. *Cargan las fotos de los usuarios segun su viaje.

--- PlanificadorViajes/SitioWeb/static/JS/detalle_viaje.js
+++ PlanificadorViajes/SitioWeb/static/JS/detalle_viaje.js
@@ -4,4 +4,32 @@ btn_invitar.addEventListener('click', () => {
     let correo = correoInput.value.trim();
 })
 
+let contador_movimiento = 0;
+
+const btn_prev = document.querySelector('.btn-flecha-izq');
+const btn_sig = document.querySelector('.btn-flecha-der');
+const cont_fotos = document.getElementById('cont-imagenes');
+const numImagenes = cont_fotos.querySelectorAll('img').length;
+
+const anchoDinamico = numImagenes * 100; // Multiplica la cantidad de imágenes por 100
+
+// Aplica el ancho al contenedor
+cont_fotos.style.width = (anchoDinamico + 50) + '%';
+
+btn_prev.addEventListener('click',()=>{
+    const cont_fotos = document.querySelector('.cont-fotos');
+    if(contador_movimiento < (numImagenes * 0)){
+        contador_movimiento += (100 / numImagenes);
+        cont_fotos.style.transform = `translateX(${contador_movimiento}%)`;
+    }
+})
+
+btn_sig.addEventListener('click',()=>{
+    const cont_fotos = document.querySelector('.cont-fotos');
+    if(contador_movimiento > -100 + (100/ numImagenes)){
+        contador_movimiento -= (100 / numImagenes);
+        cont_fotos.style.transform = `translateX(${contador_movimiento}%)`;
+    }
+})
+
 
