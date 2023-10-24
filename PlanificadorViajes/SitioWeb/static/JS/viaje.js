5a9473c66db68d78383226d389c767460c002af5
Ignacio <ignacioelopez04@gmail.com>
2023-10-24 11:52:12 -0300

    *Las fotos del viaje se muestran como masaico o como se llame. *Los botones de confirmar y siguiente en 'viaje.html' no desaparecen. *Acomode los loading-overlay. *Detalles en el login.

--- PlanificadorViajes/SitioWeb/static/JS/viaje.js
+++ PlanificadorViajes/SitioWeb/static/JS/viaje.js
@@ -45,7 +45,7 @@ document.addEventListener("DOMContentLoaded", function () {
             // Crear un botón de eliminar
             var botonEliminar = document.createElement("button");
             botonEliminar.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M256 48a208 208 0 1 1 0 416 208 208 0 1 1 0-416zm0 464A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM175 175c-9.4 9.4-9.4 24.6 0 33.9l47 47-47 47c-9.4 9.4-9.4 24.6 0 33.9s24.6 9.4 33.9 0l47-47 47 47c9.4 9.4 24.6 9.4 33.9 0s9.4-24.6 0-33.9l-47-47 47-47c9.4-9.4 9.4-24.6 0-33.9s-24.6-9.4-33.9 0l-47 47-47-47c-9.4-9.4-24.6-9.4-33.9 0z"/></svg>';
-            botonEliminar.className = "btn-eliminar";
+            botonEliminar.className = "d-flex justify-content-center align-items-center btn-eliminar";
 
             // Manejar el clic en el botón de eliminar
             botonEliminar.addEventListener("click", function () {
@@ -217,12 +217,4 @@ document.addEventListener('DOMContentLoaded', function () {
             })
             .catch(error => console.error('Error al obtener los detalles del viaje:', error));
     }
-});
-
-document.addEventListener('DOMContentLoaded', function () {
-    const btnSiguiente = document.getElementById('btn-cargar-info-viaje-general');
-
-    btnSiguiente.addEventListener('click', function () {
-        btnSiguiente.style.display = 'none';
-    });
 });
