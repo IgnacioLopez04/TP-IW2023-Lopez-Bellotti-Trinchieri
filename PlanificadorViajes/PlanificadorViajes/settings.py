745744f1f85327735dcc5d55aa9db272deded9cb
Ignacio <ignacioelopez04@gmail.com>
2023-10-25 01:11:21 -0300

    Dockerfile finalizado

--- PlanificadorViajes/PlanificadorViajes/settings.py
+++ PlanificadorViajes/PlanificadorViajes/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -93,7 +94,7 @@ ROOT_URLCONF = 'PlanificadorViajes.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR, "base-templates"],
+        'DIRS': [os.path.join(BASE_DIR, "base-templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
