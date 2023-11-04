4937beee6e2760283f3f7ddb20e0e1f81329428d
LucasTrinchieri <lucastrinchieri123@gmail.com>
2023-11-04 02:58:43 -0300

    Los viajes publicos cuando entras al detalle viaje no te aparece el modal para confirmar la invitacion

--- PlanificadorViajes/PlanificadorViajes/settings.py
+++ PlanificadorViajes/PlanificadorViajes/settings.py
@@ -223,7 +223,7 @@ LOGIN_REDIRECT_URL = 'sitio-inicio'
 LOGOUT_REDIRECT_URL = 'sitio-inicio'
 
 # code needed to deploy in Render.com:
-import os
+"""import os
 import dj_database_url
 
 if 'RENDER' in os.environ:
@@ -243,3 +243,4 @@ if 'RENDER' in os.environ:
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+"""
